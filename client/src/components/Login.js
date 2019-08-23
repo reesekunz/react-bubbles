2bf95dec18fc72baa38a2b09dd72cd6b6ceebb06
Reese Kunz <reesekunz21@gmail.com>
2019-08-23 08:31:49 -0700

    built out login form, getting 403 forbidden post request error

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,58 @@
 import React from "react";
+import axios from "axios";
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+class Login extends React.Component {
+  state = {
+    loginCredentials: {
+      username: "",
+      password: ""
+    }
+  };
+
+  handleChange = event => {
+    this.setState({
+      loginCredentials: {
+        ...this.state.loginCredentials,
+        [event.target.name]: event.target.value
+      }
+    });
+  };
+
+  login = event => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:5000/api/login", this.state.loginCredentials)
+      .then(response => {
+        console.log("post request success, data = ", response)
+        // localStorage.setItem("token", response.data.payload);
+      })
+      .catch(error => console.log(error.response));
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Log in</h2>
+        <form onSubmit={this.login}>
+          <input
+            type="text"
+            name="username"
+            placeholder="username"
+            value={this.state.loginCredentials.username}
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            value={this.state.loginCredentials.password}
+            onChange={this.handleChange}
+          />
+          <button className="submit-button" type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Login;
