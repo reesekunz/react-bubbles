88eed6224292214f5b7c47c97d2c81d0907e162e
Reese Kunz <reesekunz21@gmail.com>
2019-08-23 10:55:56 -0700

    add color form working, finished http strech goal

--- client/src/components/AddColorForm.js
+++ client/src/components/AddColorForm.js
@@ -4,18 +4,30 @@ import axios from "axios";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 class AddColorForm extends React.Component {
-  state = {
-    initialColor: {
-      color: "",
-      code: { hex: "" }
-    }
-  };
+  constructor() {
+    super();
+    this.state = {
+      initialColor: {
+        color: "",
+        code: { hex: "" }
+      }
+    };
+  }
 
-  handleChange = event => {
+  handleChangeColor = event => {
     this.setState({
         initialColor: {
         ...this.state.initialColor,
         [event.target.name]: event.target.value
+      }
+    });
+  };
+
+  handleChangeHexCode = event => {
+    this.setState({
+        initialColor: {
+        ...this.state.initialColor,
+        code: { hex: event.target.value }
       }
     });
   };
@@ -25,34 +37,39 @@ class AddColorForm extends React.Component {
     axiosWithAuth()
       .post("http://localhost:5000/api/colors", this.state.initialColor)
       .then(response => {
-        console.log("add color post request success", response)
+        console.log("add color post request success", response);
       })
       .catch(error => console.log("error, sorry dood", error.response));
   };
 
   render() {
+    console.log("initialColor state", this.state.initialColor);
+    console.log("initialColor state", this.state.initialColor.color);
+    // console.log("initialColor state", this.state.initialColor.code);
+    console.log("initialColor state", this.state.initialColor.code.hex);
+
     return (
       <div>
         <h2>Add Color Form</h2>
         <form onSubmit={this.addColor}>
-            {/* Color input  */}
-            <h3>Color </h3> 
+          {/* Color input  */}
+          <h3>Color </h3>
           <input
             type="text"
             name="color"
             placeholder="color name"
             value={this.state.initialColor.color}
-            onChange={this.handleChange}
+            onChange={this.handleChangeColor}
           />
           {/* Hex color code input  */}
-          <h3>Hex Color Code</h3> 
+          <h3>Hex Color Code</h3>
           <input
             type="text"
-            name="hexcode"
-            placeholder="eg. #2e2c2e"
+            name="hex code"
+            placeholder="#e41032 "
             // value={this.state.initialColor.code}
             value={this.state.initialColor.code.hex}
-            onChange={this.handleChange}
+            onChange={this.handleChangeHexCode}
           />
           <button className="submit-button" type="submit">
             Add Color
